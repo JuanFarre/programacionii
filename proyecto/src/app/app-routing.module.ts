8c3760d2e35d3e73b9652b94976a2ee93d108783
JuanFarre <juanfarre99@gmail.com>
2024-08-22 11:19:06 -0300

    ejercicio clase

--- proyecto/src/app/app-routing.module.ts
+++ proyecto/src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ProbandoComponent } from './components/probando/probando.component';
+import { EmpleadosComponent } from './components/empleados/empleados.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'probando', component: ProbandoComponent },
+  {path: 'empleados', component: EmpleadosComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- proyecto/src/app/app.module.ts
+++ proyecto/src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ProbandoComponent } from './components/probando/probando.component';
+import { EmpleadosComponent } from './components/empleados/empleados.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ProbandoComponent
+    ProbandoComponent,
+    EmpleadosComponent
   ],
   imports: [
     BrowserModule,
